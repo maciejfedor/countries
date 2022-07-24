1adbc67d04659bdf995baa291f55fa6312b9b170
Maciej Fedor <fedor.maciej@gmail.com>
2022-07-24 15:16:59 +0200

    fetch neighbouring country data

--- script.js
+++ script.js
@@ -93,9 +93,20 @@ const renderCountry = function (data, className = '') {
 // };
 
 const getCountryData = function (country) {
+  // Country 1
   fetch(`https://restcountries.com/v3.1/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders?.[0];
+
+      if (!neighbour) return;
+
+      // Country 2
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data[0], 'neighbour'));
 };
 
 getCountryData('portugal');
